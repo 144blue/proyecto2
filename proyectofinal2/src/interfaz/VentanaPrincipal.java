66bcaf68346000ed9c5cc283b5b4601933ee4ecd
Douglas <Douglas@LAPTOP-39BP8VU3>
2018-05-08 23:13:06 -0500

    Hice un cambio profesional

---
+++ proyectofinal2/src/interfaz/VentanaPrincipal.java
@@ -0,0 +1,17 @@
+package interfaz;
+
+public class VentanaPrincipal {
+    
+	/**
+	 * Método constructor de la clase.
+	 */
+	public VentanaPrincipal() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
